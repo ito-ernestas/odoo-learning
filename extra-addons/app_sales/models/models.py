dec35137f21c7ffb465e414e70a8870498e4449a
Ernestas Pilsudskis <ernestas@ito.lt>
2023-12-05 10:01:04 +0000

    in p

--- extra-addons/app_sales/models/models.py
+++ extra-addons/app_sales/models/models.py
@@ -1,7 +1,9 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class WorkOrderType(models.Model):
-    _name = 'work.order_type'
+    _name = 'work_order_type'
     _description = 'Work Order Type'
 
     name = fields.Char(string='Work Order Type', required=True)
@@ -10,3 +12,27 @@ class WorkOrderType(models.Model):
         ('standard', 'Standard'),
         ('urgent', 'Urgent')
     ], string='Priority', default='standard', required=True)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    partner_object_id = fields.Many2one(
+        'partner.object', string='Object', required=False
+    )
+
+    work_order_type_id = fields.Many2one(
+        'work_order_type', string='Work Order Type', required=False
+    )
+
+    @api.constrains('state')
+    def check_work_order_type(self):
+        for record in self:
+            if record.state == 'sale' and not record.work_order_type_id:
+                raise ValidationError("Work Order Type is required to confirm the order.")
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        if not self.partner_id:
+            return
+        return {'domain': {'partner_object_id': [('partner_id', '=', self.partner_id)]}}
